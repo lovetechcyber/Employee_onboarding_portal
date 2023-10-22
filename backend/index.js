2a9046f734abf0fcc03a7bd115dfc02f1dfa374e
Kelvin Akaba <akabakelvin@gmail.com>
2023-10-22 14:02:26 +0000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 5000
+
+
+
+server.listen(PORT, () => console.log(`The Server is live on Port ${PORT} `))
