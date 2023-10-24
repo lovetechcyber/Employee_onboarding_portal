92b5a0a997601c1930eabadfea2ab72580dc06b1
Kelvin Akaba <akabakelvin@gmail.com>
2023-10-24 01:21:59 +0000

    layout commit

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='flex'>
-      {/* Add a margin on the left to create space for the Sidebar */}
-      <div className='w-1/5'></div>
-      <div className='flex-1 justify-center items-center'>
-        Dashboard content
-      </div>
+    <div className=''>
+      Dashboard
     </div>
   )
 }
