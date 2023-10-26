41408eaf641565d1586387c767d09960db68b140
tonisco <tonyumeozulu@gmail.com>
2023-10-26 12:27:19 +0100

    express tools

--- employee-dashboard/backend/src/index.js
+++ employee-dashboard/backend/src/index.js
@@ -5,6 +5,13 @@ const connectDb = require("./utils/connectDb.js")
 
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+app.use(compression())
+app.use(helmet())
+
+if (process.env.NODE_ENV !== "production") app.use(morgan("dev"))
+
 connectDb()
 
 const PORT = process.env.PORT || 5000
