26e49858f96aa0f62fc66342696331ddbda59c3c
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-08 11:25:05 +0000

    chat button

--- employee-dashboard/frontend/src/App.jsx
+++ employee-dashboard/frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './Pages/Home'
 import Feed from './Pages/Feed'
 import People from './Pages/People'
 import Team from './Pages/Team'
+import { BsChatDotsFill } from "react-icons/bs";
+
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           <Route path='/people' element={<People />} />
           <Route path='/team' element={<Team />} />
         </Routes>
+        <div className='fixed right-6 bottom-6'>
+          <BsChatDotsFill size={40} />
+        </div>
       </Router>
     </>
   )
