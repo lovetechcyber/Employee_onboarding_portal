cf14f4cead5bb55bfb73258e09168d289bc70c9f
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-14 22:29:34 +0000

    database connection

--- hr-dashboard/backend/index.js
+++ hr-dashboard/backend/index.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
+
+const connectDatabase = require('./src/config/database')
+connectDatabase()
+
+server.use(cors())
+server.use(express.json())
 
 
 
---
+++ hr-dashboard/backend/src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const database = process.env.DATABASE
+
+const connectDatabase = async() => {
+    try {
+        const db = await mongoose.connect(database)
+        console.log(`Database linked to ${db.connection.host}`);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error)
+    }
+}
+
+module.exports = connectDatabase
