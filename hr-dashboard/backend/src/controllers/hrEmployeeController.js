2ab1666d0025a43fce4e87f8ad3eca71b000f7be
Kelvin Akaba <akabakelvin@gmail.com>
2023-11-15 23:45:48 +0000

    create/get employess

--- hr-dashboard/backend/src/controllers/hrEmployeeController.js
+++ hr-dashboard/backend/src/controllers/hrEmployeeController.js
@@ -2,17 +2,34 @@ const hrEmployee = require('../models/employee')
 
 const registerHrEmployee = async(req, res) => {
     try {
-        
+        const { fullName, email } = req.body
+        if (!fullName || !email) {
+            return res.status(400).send({ success: false, message: "Please Complete All Fields"})
+        }
+        const employee = await hrEmployee.create(req.body)
+        res.status(200).json({ success: true, message: 'Employee Registered Succ', employee})
+
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({
+        success: false,
+        message: 'Error completing registration',
+        error: error.message,
+    });
     }
 }
 
 const getAllEmployees = async(req, res) => {
     try {
-        
+        const employees = await hrEmployee.find({ createdBy: req.user.userId})
+        res.status(200).json({ totalEmployees: employees.length, employees})
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({
+        success: false,
+        message: 'Error completing registration',
+        error: error.message,
+        });
     }
 }
 
@@ -25,3 +42,12 @@ const getEmployeeDetails = async(req, res) => {
 }
 
 module.exports = { registerHrEmployee, getAllEmployees, getEmployeeDetails}
+
+ // const user = await User.findById(req.user.userId);
+    // user.additionalDetails = additionalDetails;
+    // const updatedUser = await user.save();
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'Registration completed successfully',
+    //   user: updatedUser,
+    // });
