123ec3d5f9f964a366dad3d32f20e0cde35d9828
tonisco <tonyumeozulu@gmail.com>
2023-11-17 15:38:51 +0100

    auth changes

--- hr-dashboard/backend/src/middlewares/authMiddleware.js
+++ hr-dashboard/backend/src/middlewares/authMiddleware.js
@@ -1,21 +1,26 @@
-const JWT = require('jsonwebtoken')
+const JWT = require("jsonwebtoken")
 const jwtKey = process.env.JWT_SECRET
 
-const userAuth = async(req, res) => {
-    const authHeader = req.headers.authorization
-    if (!authHeader || !authHeader.startsWith('Bearer')) {
-        return res.status(401).send({ success: false, message: "Authentication Failed"})
-    }
+const userAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Authentication Failed" })
+  }
 
-    const token = authHeader.split(' ')[1]
+  const token = authHeader.split(" ")[1]
 
-    try {
-        const payload = JWT.verify(token, jwtKey)
-        req.body.user = { userId: payload.userId }
-        return res.status(200).send({ success: true, message: "Authentication Success"})
-    } catch (error) {
-        return res.status(400).send({ success: false, message: "Authentication Failed"})
-    }
+  try {
+    const payload = JWT.verify(token, jwtKey)
+    // req.body.user = { userId: payload.userId }
+    req.userId = payload.userId
+    next()
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Authentication Failed" })
+  }
 }
 
 module.exports = userAuth
