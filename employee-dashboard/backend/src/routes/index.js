cc8f21725293b71e97790bcdd2241428e98778cc
Chukwuebuka James Anunihu <anunihuebuka01@gmail.com>
2023-11-21 22:52:56 +0100

    pushing only hr dashboard

--- employee-dashboard/backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-
-const { loginHandler } = require("../controllers/auth")
-
-const router = Router()
-
-router.post("/login", loginHandler)
-
-module.exports = router
